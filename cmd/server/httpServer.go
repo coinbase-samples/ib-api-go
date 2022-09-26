24112805125e5dafd0609a5c4f5123cdbb896005
Carl Fluke <carl.fluke@coinbase.com>
2022-09-26 19:44:08 -0400

    try prod with port 8449

--- cmd/server/httpServer.go
+++ cmd/server/httpServer.go
@@ -30,7 +30,7 @@ func getProfileConnAddress(app config.AppConfig) string {
 	if app.Env == "local" {
 		return fmt.Sprintf("%s:%s", "0.0.0.0", app.GrpcPort)
 	}
-	return fmt.Sprintf("%s:443", "api-dev.neoworks.xyz")
+	return fmt.Sprintf("%s:%s", "api-dev.neoworks.xyz", app.GrpcPort)
 }
 
 func testOrderDial(app config.AppConfig) {
