e08af41d2d1c4ccda35350afaf2e73f38cb9ec09
Carl Fluke <carl.fluke@coinbase.com>
2022-09-26 19:56:52 -0400

    try port by os hostname

--- cmd/server/httpServer.go
+++ cmd/server/httpServer.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 	"time"
 
@@ -30,7 +31,8 @@ func getProfileConnAddress(app config.AppConfig) string {
 	if app.Env == "local" {
 		return fmt.Sprintf("%s:%s", "0.0.0.0", app.GrpcPort)
 	}
-	return fmt.Sprintf("%s:%s", "api-dev.neoworks.xyz", app.GrpcPort)
+	name, _ := os.Hostname()
+	return fmt.Sprintf("%s:%s", name, app.GrpcPort)
 }
 
 func testOrderDial(app config.AppConfig) {
